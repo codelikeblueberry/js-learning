72bb8bf76c7cb410bde968296552195909ed52b6
Swatantra <102201952+codelikeblueberry@users.noreply.github.com>
2024-02-02 16:20:47 +0530

    added 01_varibles.js file in 01_basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const name = "Blueberry" //immutable
+let id = 22323 // half immutable
+var city = "JHANSI" //mutable 
+country = "INDIA" // mutable
+let planet;
+// let id = 123 this is giving error meaning : it can't be letted but can be redefiend by ; id=123
+// const name = 123 ; cannot declare block varible in both ways ; name = 123 or const name = "aman"
+
+id = 123
+city = "UP"
+country = "china"
+
+console.table([name,id,city,country,planet]);
+
+/* 
+use the const, let, for defining constants and varibles
+*/  
