ef965a74fb91a45c562740a72e07b6c8b8da84eb
Swatantra <102201952+codelikeblueberry@users.noreply.github.com>
2024-02-08 00:13:50 +0530

    practiced convertion_operations.js

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,13 @@
+//let score = null => 0,number
+//let score = "22abc" => NotaNumber,number
+//let score = undefined => NaN , number
+//let score = true => 1, number
+//let score = "" => false , Boolean(score)
+let score;
+console.log(typeof(score));
+// convert score variable into integer
+// let new_score =Number(score)
+let new_score = String(score)
+//console.log("Your score is",new_score,"and type is",typeof(new_score));
+console.log(typeof(new_score));
+console.log(new_score);
