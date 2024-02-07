7c15d79f538b6685e121f92fed42ffaf8e96736f
Swatantra <102201952+codelikeblueberry@users.noreply.github.com>
2024-02-07 23:59:20 +0530

    practice something in datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+let name = "Shahrukh"
+let number = 12
+let boolean = true
+let underdefined;
+// bigint
+// string = ""
+// boolean = true/false
+// null : stand-alone value ; its a data type
+// undefined : print(underdefiend)
+// console.log(underdefined); you will get undefined
+// symbol => unique
+
+// object ; itminaan se padenge
+
+// typeof is like type in python
+console.log(typeof('helloworld'));
+console.log(typeof(null)); // object
+console.log(typeof(underdefined)); // undefined
+
+/*
+What i have learned today?
+1. the null data type
+2. undefined data type
+3. typeof() function
+*/
