4a0fe589bed91e36d64c5a3a15e782fcb3031207
Swatantra <102201952+codelikeblueberry@users.noreply.github.com>
2024-03-06 10:40:47 +0530

    IFFE functions

---
+++ 04_functions/IFFE.js
@@ -0,0 +1,27 @@
+// unnamed iffe arrow
+// *****************************************************************************************
+(()=>{
+    console.log("unnamed iffe arrow");
+})();
+// named iffe arrow
+
+const vai = (()=>{
+     console.log("named iffe arrow");
+})();
+
+
+// **********************************************************************************************
+// named iffe simple function
+
+(function simple(){
+     console.log("named simple iffe");
+})();
+
+// unnamed iffe simple function
+(function (){
+        console.log("unnamed simple iffe");
+})();
+
+// ************************************************************************************************
+
+
